694eed6875398a8a51517d371041914d61810bfa
swensson <lxswensson@gmail.com>
2022-04-19 19:26:16 +0200

    Refactor

--- web-client/src/components/molecules/PluggableEditor/boot.js
+++ web-client/src/components/molecules/PluggableEditor/boot.js
@@ -1,27 +1,23 @@
-import Modes from './modes';
-import Canvas from './canvas';
-import Debug from './debug';
-import Events from './events';
+import Modes from './core/modes-manager';
+import Canvas from './core/canvas-manager';
+import Events from './core/events-manager';
+import Program from './core/program-state';
 
-import DragScale from './drag-scale';
+import DragScale from './view/drag-scale';
+import ProgramRender from './view/program-render';
 
-import Program from './program';
-import ProgramRender from './program-render';
-
-import Selection from './selection';
+// import Selection from './selection';
 
 export default (node) => {
   const modes = new Modes();
   const program = new Program();
-
   const canvas = new Canvas(node);
   const events = new Events(node);
   // const debug = new Debug(canvas);
 
   const dragScale = new DragScale(modes, canvas, events, program);
-
   const programRender = new ProgramRender(dragScale, program);
-  const selection = new Selection(modes, canvas, dragScale, events);
+  // const selection = new Selection(modes, canvas, dragScale, events);
 
   const dispose = () => {
     canvas.dispose();
